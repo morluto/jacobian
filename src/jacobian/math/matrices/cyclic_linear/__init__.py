3fd0e5834d11af9cc49e6a625170e829f58ffb65
morluto <76467478+morluto@users.noreply.github.com>
2026-09-25 05:07:54 +0800

    feat(number-theory): add standard cyclotomic inclusions

--- src/jacobian/math/matrices/cyclic_linear/__init__.py
+++ src/jacobian/math/matrices/cyclic_linear/__init__.py
@@ -4,6 +4,10 @@ from jacobian.math.matrices.cyclic_linear._models import (
     CyclicRationalBlockSymbol,
     CyclicRationalBlockSymbolEntry,
     CyclicRationalRankKernelProfile,
+    CyclotomicElementMapRequest,
+    CyclotomicFieldInclusion,
+    CyclotomicFieldInclusionCompositionRequest,
+    CyclotomicFieldInclusionRequest,
     CyclotomicNonzeroMinor,
     CyclotomicRankKernelComponent,
     RationalCyclotomicElement,
@@ -12,7 +16,10 @@ from jacobian.math.matrices.cyclic_linear._models import (
     RationalCyclotomicVectorSpaceBasis,
 )
 from jacobian.math.matrices.cyclic_linear.operations import (
+    apply_cyclotomic_field_inclusion,
+    compose_cyclotomic_field_inclusions,
     cyclic_rational_rank_kernel_profile,
+    cyclotomic_field_inclusion,
     verify_cyclic_rational_rank_kernel_profile,
 )
 
@@ -20,12 +27,19 @@ __all__ = [
     "CyclicRationalBlockSymbol",
     "CyclicRationalBlockSymbolEntry",
     "CyclicRationalRankKernelProfile",
+    "CyclotomicElementMapRequest",
+    "CyclotomicFieldInclusion",
+    "CyclotomicFieldInclusionCompositionRequest",
+    "CyclotomicFieldInclusionRequest",
     "CyclotomicNonzeroMinor",
     "CyclotomicRankKernelComponent",
     "RationalCyclotomicElement",
     "RationalCyclotomicField",
     "RationalCyclotomicMatrix",
     "RationalCyclotomicVectorSpaceBasis",
+    "apply_cyclotomic_field_inclusion",
+    "compose_cyclotomic_field_inclusions",
     "cyclic_rational_rank_kernel_profile",
+    "cyclotomic_field_inclusion",
     "verify_cyclic_rational_rank_kernel_profile",
 ]
